ffd31316ba89911017877b05537e1b5d08061fac
平凡 <849260995@qq.com>
2017-09-05 23:26:43 +0800

    1

---
+++ TencentTestDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TencentTestDemo
+//
+//  Created by Yahaw Li on 17/9/2.
+//  Copyright © 2017年 Yahaw Lee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ TencentTestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TencentTestDemo
+//
+//  Created by Yahaw Li on 17/9/2.
+//  Copyright © 2017年 Yahaw Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
